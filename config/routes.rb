13417b4bcc0903ade77cc06caa35e7d7f838fe26
Jean Luc Abayo <luc.bayo@gmail.com>
2017-01-24 18:27:09 +0200

    modifed contact app using Rest convention

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get "/contacts", to: 'contacts#index'
+  get "/contacts/new", to: 'contacts#new'
+  get "/contacts/:id", to: 'contacts#show'
+  post "/contacts", to: 'contacts#create'
+  get "/contacts/:id/edit", to: 'contacts#edit'
+  patch "/contacts/:id", to: 'contacts#update'
+  delete "/contacts/:id", to: 'contacts#destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
